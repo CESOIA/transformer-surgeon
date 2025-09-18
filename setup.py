4ef80fb8b2a2c4c26cb7049cbf2e8b69c01a0e82
prolucio <luciano.prono@polito.it>
2025-09-18 12:12:18 +0200

    Updated versioning + small test update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="transformer-surgeon",
-    version="0.1.2",
+    version="0.2.0",
     description="Transformer model compression and optimization tools",
     packages=find_packages(),
     python_requires=">=3.8",
